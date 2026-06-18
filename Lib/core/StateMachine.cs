91c498971b3c1dd707d508353bade2983bf22fa7
Wild-Mane <fizlevine@gmail.com>
2026-06-19 00:08:18 +0700

    tambah core module

---
+++ Lib/core/StateMachine.cs
@@ -0,0 +1,22 @@
+﻿using System;
+
+namespace Lib.core
+{
+    public enum State
+    {
+        Welcome,
+        Login,
+        Register,
+        Home,
+        Admin,
+        ViewPaper,
+        Exit
+    }
+
+    public class StateMachine
+    {
+        public State Current { get; private set; } = State.Welcome;
+
+        public void Set(State s) => Current = s;
+    }
+}
